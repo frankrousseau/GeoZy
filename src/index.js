1ba7ddbd530374e2e98d0e5d801f3973a3f27ddc
RobyRemzy <farnwarlock@openmailbox.org>
2015-05-10 21:13:58 +0200

    Add new content

--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@
 */
 var React = require('React');
 var UpperPanel = require('./elements/upperpanel/UpperPanel.js');
-
-var skull = require('./core/skull.js');
 var SiteBoilerPlate = require('./core/SiteBoilerPlate.js');
-//var ZeMap = require('./elements/map/ZeMap.js');
-//var MapBox = require('./core/MapBox.js');
-//var VectorWidget = require('./elements/VectorWidget/VectorWidget.js');
-//var Alert = require('react-bootstrap/lib/Alert');
+
 
 var index = React.createClass({
   render: function() {
